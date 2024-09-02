1baabe73c15bb3e7653117edf84d574338ec6977
randytsao24 <randytsao24@gmail.com>
2024-09-02 17:18:16 -0400

    Increase opacity on ps and add some info

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,10 +4,10 @@ const About: React.FC = () => {
   return (
     <section>
       <div className="container mx-auto px-4">
-        <div className='m-6 w-4/5 md:w-1/2 mx-auto flex flex-col bg-zinc-50 border-2 border-stone-200 rounded-xl shadow-lg opacity-60 text-black'>
+        <div className='m-6 w-full md:w-4/5 mx-auto flex flex-col bg-zinc-50 border-2 border-stone-200 rounded-xl shadow-lg opacity-66 text-black'>
           <h2 className="p-4 text-3xl font-extrabold underline">Welcome!</h2>
           <p className="p-4 text-lg font-mono">
-            Thanks for dropping by my site! I'm a software developer from sunny Southern California, and I'm currently based in Queens. I like web frameworks, dev tools, and obscure code editor themes.
+            Thanks for dropping by my site! I'm a software developer from sunny Southern California, and I'm currently based in Queens, New York. I like web frameworks, dev tools, and obscure code editor themes.
           </p>
           <p className="p-4 text-lg font-mono">
             Reach out to me at
@@ -33,10 +33,17 @@ const About: React.FC = () => {
             .
           </p>
         </div>
-        <div className='m-6 w-4/5 md:w-1/2 mx-auto flex flex-col bg-zinc-50 border-2 border-stone-200 rounded-xl shadow-lg opacity-60 text-black'>
-          <h2 className="p-4 text-3xl font-extrabold underline">More to come...</h2>
+        <div className='m-6 w-full md:w-4/5 mx-auto flex flex-col bg-zinc-50 border-2 border-stone-200 rounded-xl shadow-lg opacity-66 text-black'>
+          <h2 className="p-4 text-3xl font-extrabold underline">About the site...</h2>
           <p className="p-4 text-lg font-mono">
             I'm currently dabbling in game development (specifically the Godot game engine), and the plan is to host some kind of 2D game on this site along with some random blogposts about stuff I like, such as music, games, history, or travels.
+          </p>
+          <p className="p-4 text-lg font-mono">
+            This site was built in{' '}
+            <span className='text-blue-700 underline'>
+              <a href='https://www.cursor.com/'>Cursor</a>
+            </span>
+            , a very nifty AI-first code editor which provides a pretty novel way to write code, especially when using Claude 3.5 Sonnet. This site is basically going to be a way for me to get acquainted with the brave new world of AI-driven software development.
           </p>
         </div>
       </div>
--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -11,7 +11,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
     <>
     <Header />
       <main className="flex-grow bg-gradient-to-br from-amber-100 via-yellow-50 to-amber-50">
-        <div className="container mx-auto px-4 py-20 pt-24">
+        <div className="container mx-auto px-4 py-16 pt-20">
           <div className="flex flex-col items-center justify-center">
             {children}
           </div>
