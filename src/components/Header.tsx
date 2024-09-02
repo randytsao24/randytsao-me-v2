caca41e65ee0df73c7eb4bd07c4da604a5ee87d3
randytsao24 <randytsao24@gmail.com>
2024-09-02 18:12:02 -0400

    Adjust header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,20 @@ import { Link } from "gatsby";
 
 const Header: React.FC = () => {
   return (
-    <header className="text-gray-800 py-4 px-8 fixed w-full z-10">
+    <header className="text-gray-800 py-4 px-8 fixed w-full z-10 mt-2">
       <div className="mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold font-mono">
-          <Link to="/" className="transition duration-300 ease-in-out hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 rounded px-2 py-1">
+        <h1 className="text-2xl font-bold font-mono relative">
+          <Link 
+            to="/" 
+            className="
+              transition duration-300 ease-in-out 
+              hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 
+              rounded px-3 py-2
+              border-2 border-black
+              shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]
+              hover:shadow-none hover:translate-x-[5px] hover:translate-y-[5px]
+            "
+          >
             Randy Tsao
           </Link>
         </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,24 @@ module.exports = {
       fontFamily: {
         'mono': ['"Roboto Mono"', 'monospace'],
       },
+      animation: {
+        'flash': 'flash 1s ease-in-out infinite',
+        'draw-circle': 'draw-circle 2s ease-in-out infinite',
+      },
+      keyframes: {
+        flash: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0.5' },
+        },
+        'draw-circle': {
+          '0%': { strokeDasharray: '0 69', strokeDashoffset: '0' },
+          '100%': { strokeDasharray: '69 69', strokeDashoffset: '0' },
+        }
+      }
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 }
