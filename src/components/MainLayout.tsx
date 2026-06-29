2d4daf18e5efd2473b1d991483f8f936593d11b1
Randy Tsao <randytsao24@gmail.com>
2026-06-28 20:20:14 -0400

    Remove Three.js

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,8 +1,6 @@
 import React, { FC, ReactNode } from "react";
 
 import Header from "./Header";
-import ThreeBackground from "./ThreeBackground";
-import ClientOnly from "./ClientOnly";
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -11,9 +9,6 @@ interface MainLayoutProps {
 const MainLayout: FC<MainLayoutProps> = ({ children }) => {
   return (
     <>
-      <ClientOnly>
-        <ThreeBackground />
-      </ClientOnly>
       <Header />
       <main className="flex-grow bg-gradient-to-br from-amber-100/80 via-yellow-50/70 to-amber-50/80 relative">
         <div className="container mx-auto px-4 py-16 pt-20">
